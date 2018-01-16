7c575cf7c398eed6a857db1ccbb05e3c7c53703d
Hayden Gill <hjgill7@gmail.com>
2018-01-16 22:44:39 +0000

    Add project files.

---
+++ Files/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Files
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.sabtn1 = new System.Windows.Forms.Button();
+            this.opbtn1 = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.pbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // sabtn1
+            // 
+            this.sabtn1.Location = new System.Drawing.Point(197, 100);
+            this.sabtn1.Name = "sabtn1";
+            this.sabtn1.Size = new System.Drawing.Size(75, 23);
+            this.sabtn1.TabIndex = 0;
+            this.sabtn1.Text = "Save As";
+            this.sabtn1.UseVisualStyleBackColor = true;
+            this.sabtn1.Click += new System.EventHandler(this.sabtn1_Click);
+            // 
+            // opbtn1
+            // 
+            this.opbtn1.Location = new System.Drawing.Point(197, 71);
+            this.opbtn1.Name = "opbtn1";
+            this.opbtn1.Size = new System.Drawing.Size(75, 23);
+            this.opbtn1.TabIndex = 1;
+            this.opbtn1.Text = "Open";
+            this.opbtn1.UseVisualStyleBackColor = true;
+            this.opbtn1.Click += new System.EventHandler(this.opbtn1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(12, 50);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(179, 156);
+            this.tb1.TabIndex = 2;
+            this.tb1.TextChanged += new System.EventHandler(this.tb1_TextChanged);
+            // 
+            // pbtn
+            // 
+            this.pbtn.Location = new System.Drawing.Point(55, 226);
+            this.pbtn.Name = "pbtn";
+            this.pbtn.Size = new System.Drawing.Size(75, 23);
+            this.pbtn.TabIndex = 3;
+            this.pbtn.Text = "Play";
+            this.pbtn.UseVisualStyleBackColor = true;
+            this.pbtn.Click += new System.EventHandler(this.pbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pbtn);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.opbtn1);
+            this.Controls.Add(this.sabtn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button sabtn1;
+        private System.Windows.Forms.Button opbtn1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Button pbtn;
+    }
+}
+
---
+++ Files/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+
+namespace Files
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //changes for github
+        }
+
+        private void sabtn1_Click(object sender, EventArgs e)
+        {
+            //Display a SaveFileDialog so the user can save the Image
+            //Assigned to sabtn1
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File|*.txt";
+            saveFileDialog.Title = "Save an Text File";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, tb1.Text);
+            }            
+        }
+
+        public void opbtn1_Click(object sender, EventArgs e)
+        {
+            //Display OpenFileDialog so the user can open a text file
+            //Assigned to opbtn1
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "E:\\Code\\";
+            openFileDialog1.Filter = "Text File|*.txt";
+            openFileDialog1.Title = "Open Text File";
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.ShowDialog();
+
+            tb1.Text = File.ReadAllText(openFileDialog1.FileName);
+        }
+
+        public void tb1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void pbtn_Click(object sender, EventArgs e)
+        {
+            SoundPlayer play = new SoundPlayer(@"E:\\Code\\Windows application\\Files\\Play.wav");
+
+            play.Play();
+        }
+    }
+}
