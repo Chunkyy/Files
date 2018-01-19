68fcd57e405709998ffd091a307ec88d4253a29e
Hayden Gill <hjgill7@gmail.com>
2018-01-19 14:05:07 +0000

    fix red colour

--- Files/Form1.cs
+++ Files/Form1.cs
@@ -54,7 +54,7 @@ namespace Files
             SoundPlayer play = new SoundPlayer(@"E:\\Code\\Windows application\\Files\\Play.wav");
 
             play.Play();
-            pbtn.BackColor = Color.red;
+            pbtn.BackColor = Color.Red;
         }
     }
 }
